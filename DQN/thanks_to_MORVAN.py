793c92839c3f7d6356c9f5014105a161e3329ab1
wuyifanisai <34185002+wuyifanisai@users.noreply.github.com>
2017-12-17 17:06:34 +0800

    Update thanks_to_MORVAN.py

--- DQN/thanks_to_MORVAN.py
+++ DQN/thanks_to_MORVAN.py
@@ -2,4 +2,5 @@
 DQN code in my github is edited based on the MORVAN's code 
 MORVAN shared wonderful code with others to help everyone just like you and me 
 big thanks to MORVAN
+https://morvanzhou.github.io/
 '''
