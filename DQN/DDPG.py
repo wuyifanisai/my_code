03331320d4d27d258c211442f3952f8110babb3e
wyf <wuyifan_office@163.com>
2017-12-21 22:48:36 +0800

    DDPG add save and restore

--- DQN/DDPG.py
+++ DQN/DDPG.py
@@ -11,6 +11,8 @@ when it was a not bad statement, the actor_net could output a nice action to kee
 but when it was a bad statement, somtimes, the actor_net output a not good action, so it was a bit hard to transform a bad 
 statement to a good one
 '''
+
+
 ######################## import library #####################
 
 import tensorflow as tf 
@@ -19,20 +21,23 @@ import gym as gym
 
 ################ hyper parameters ####################
 
-MAX_EPISODES = 300  # max number of round
+MAX_EPISODES = 30  # max number of round
 MAX_EP_STEPS = 200  # max number of steps in a round
 LR_A = 0.001 # learning rate for actor eva net 
-LR_C = 0.002 # learning rate for critic eva net
+LR_C = 0.001 # learning rate for critic eva net
 GAMMA = 0.9 # reward discount
 TAU = 0.01  # soft replacement
 MEMORY_SIZE = 10000
 BATCH_SIZE = 32
 RENDER = False
 ENV_NAME = 'Pendulum-v0'
+LOAD = True 
+# False ==> train and save para  True==>restore the para and play
 
 ################## define class of DDPG ##################
 class DDPG(object):
-	def __init__(self, a_dim, s_dim, a_top):
+	def __init__(self, a_dim, s_dim, a_top,load=False):
+		self.load = load
 	# dimision of actions , statement, and limit of action
 	
 	# in the __init__ method, variable without "self" a_next, q, q_next is considered as the output from net for 
@@ -124,7 +129,15 @@ class DDPG(object):
 		a_loss = -tf.reduce_mean(q) # loss of actor eval net
 		self.atrain = tf.train.AdamOptimizer(LR_A).minimize(a_loss, var_list=self.ae_params)
 
-		self.sess.run(tf.global_variables_initializer())
+		if self.load:
+			saver = tf.train.Saver()
+			saver.restore(self.sess ,"f://DDPG.\\DDPG.ckpt")
+			print('loaded the para !')
+		else:
+			saver = tf.train.Saver()
+			saver.restore(self.sess ,"f://DDPG.\\DDPG.ckpt")
+			print('loaded the para and train again ! ! !')
+			#self.sess.run(tf.global_variables_initializer())
         # when __init__ of class DDPG is executed, all the variable were defined, 
         # and it is time to run initializer()
 
@@ -195,6 +208,13 @@ class DDPG(object):
 			q_value = tf.matmul(net, w2) + b2
 			return q_value  # Q(s,a)
 
+	def save_para(self):
+		# store -----
+		saver = tf.train.Saver()
+		save_path = saver.save(self.sess, "f://DDPG//DDPG.ckpt")
+		print('save to path:' , save_path)
+
+
 
 
 ######################## RL training #######################################
@@ -207,59 +227,68 @@ a_dim = env.action_space.shape[0]
 a_bound = env.action_space.high
 print(a_bound)
 
+ddpg = DDPG(a_dim, s_dim, a_bound,load = LOAD)  #load = True means use the para saved befoe
 
-ddpg = DDPG(a_dim, s_dim, a_bound)
-
-var=3
+var=1
 for i in range(MAX_EPISODES):
 	print("round-->",i)
 	s = env.reset()
 	ep_reward = 0
 	for j in range(MAX_EP_STEPS):
 		print("step-->",j)
-		if RENDER:
+
+		if LOAD:  # just play , do not learn
 			env.render()
 
-		a = ddpg.choose_action(s)
+			a = ddpg.choose_action(s)
+			a = [np.clip(np.random.normal(a, var), -2, 2)] 
+			# add randomness to action selection for exploration
+			print(a)
 
-		a = [np.clip(np.random.normal(a, var), -2, 2)] 
-		# add randomness to action selection for exploration
-		print(a)
+			s_, r, done, info = env.step(a)
+			s = s_
+			ep_reward = r
+			print('Episode:', i, ' Reward: %i' % int(ep_reward))
 
-		s_, r, done, info = env.step(a)
-		#print(s, s_)
+
+		else:  # play and learn 
+
+			if RENDER:
+				env.render()
+
+			a = ddpg.choose_action(s)
+
+			a = [np.clip(np.random.normal(a, var), -2, 2)] 
+			# add randomness to action selection for exploration
+			print(a)
+
+			s_, r, done, info = env.step(a)
+			#print(s, s_)
 		
-		if r>-3:
-			r=r+3
+			if r>-3:
+				r=r+3
 		
-		ddpg.store_memory(s, a, r/10, s_)
+			ddpg.store_memory(s, a, r/10, s_)
 
-		if ddpg.pointer > MEMORY_SIZE:
-			var *= .9995
+			if ddpg.pointer > MEMORY_SIZE:
+				var *= .9995
 			
-			ddpg.learn()
-			print("DDPG learn !")
-			ddpg.replacement()
-			print("replacement!")
+				ddpg.learn()
+				print("DDPG learn !")
+				ddpg.replacement()
+				print("replacement!")
 
-		s = s_
-		ep_reward = r
+			s = s_
+			ep_reward = r
 
 		
-		print('Episode:', i, ' Reward: %i' % int(ep_reward))
-		#print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var)
-		if i > 190:RENDER = True
-		if j == MAX_EP_STEPS-1:
-			break
+			print('Episode:', i, ' Reward: %i' % int(ep_reward))
+			#print('Episode:', i, ' Reward: %i' % int(ep_reward), 'Explore: %.2f' % var)
+			if i > 10:RENDER = True
+			if j == MAX_EP_STEPS-1:
+				break
 
-
-
-
-
-
-
-
-
+ddpg.save_para()
 
 
 
