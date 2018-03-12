1d213448741f89dceae242250e86ff5982adf07b
wuyifan <wuyifan_office@163.com>
2018-03-12 12:48:10 +0800

    update

--- java_demo/demo/src/demo/demo.java
+++ java_demo/demo/src/demo/demo.java
@@ -15,16 +15,36 @@ public class demo {
 	
 	public static void main(String args[]){
 		
-		class People{//因为是在主方法之内定义的，所以不用需要static关键词，以便主方法中可调用
+		class People<T>{//因为是在主方法之内定义的，所以不用需要static关键词，以便主方法中可调用
+			//定义了一个泛型类
 			int age;
 			String name;
+			private T t;//定义一个泛型
+			
 			public People(int age, String name){ //构造方法
 				this.age = age;
 				this.name = name;
 			}
+			
+			public void add(T t){
+				this.t = t;
+			}
+			
+			public T get(){
+				return this.t;
+			}
+			
 			public void hunting(Animal animal){ //类中的普通无输出方法
 				System.out.println("a human named "+this.name+" is hunting a "+animal.name);
 			}
+			
+			public <E> void fun(E[] array){ //定义一个泛型方法
+				for (E element : array){
+					System.out.println(element);
+				}
+			}
+			
+			
 		}
 		
 		//main 中的first主要执行内容
@@ -34,8 +54,15 @@ public class demo {
 		amy.eat("fish");
 		amy.live(new Live_cat());
 		snoopy.live(new Live_dog());
+		
 		System.out.println("now human is doing something bad...");
-		People people = new People(33, "Tom");
+		People people = new People<String>(33, "Tom");
+		people.add("this man is a bad guy!");
+		System.out.println(people.get());
+		
+		String[] s = {"black man ","white man ","yellow man "};
+		people.fun(s);
+		
 		people.hunting(new Animal(4,"white","female","jack"));
 		animal_protected();//调用定义在在主方法外部的方法
 		
