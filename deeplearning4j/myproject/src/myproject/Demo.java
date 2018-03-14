ac0c36e97e00a77cd4f3fddd3d040300d171d274
wuyifanisai <34185002+wuyifanisai@users.noreply.github.com>
2018-03-14 15:07:13 +0800

    Update Demo.java

--- deeplearning4j/myproject/src/myproject/Demo.java
+++ deeplearning4j/myproject/src/myproject/Demo.java
@@ -1,4 +1,10 @@
-package myproject;
+package example;
+
+/*
+ * a demo of network using dl4j library 
+ * author: wuyifan
+ */
+
 import org.datavec.api.records.reader.RecordReader;
 import org.datavec.api.records.reader.impl.csv.CSVRecordReader;
 import org.datavec.api.split.FileSplit;
@@ -23,87 +29,97 @@ import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 
 import java.io.File;
 
-/**
- * "Linear" Data Classification Example
- *
- * Based on the data from Jason Baldridge:
- * https://github.com/jasonbaldridge/try-tf/tree/master/simdata
- *
- * @author Josh Patterson
- * @author Alex Black (added plots)
- *
- */
-public class Demo {
-
-
-    public static void main(String[] args) throws Exception {
-        int seed = 123;
-        double learningRate = 0.01;
-        int batchSize = 50;
-        int nEpochs = 30;
-
-        int numInputs = 2;
-        int numOutputs = 2;
-        int numHiddenNodes = 20;
-        
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(seed)
-                .iterations(1)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .learningRate(learningRate)
-                .updater(Updater.NESTEROVS)     //To configure: .updater(new Nesterovs(0.9))
-                .list()
-                .layer(0, new DenseLayer.Builder().nIn(numInputs).nOut(numHiddenNodes)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation(Activation.RELU)
-                        .build())
-                .layer(1, new OutputLayer.Builder(LossFunction.NEGATIVELOGLIKELIHOOD)
-                        .weightInit(WeightInit.XAVIER)
-                        .activation(Activation.SOFTMAX).weightInit(WeightInit.XAVIER)
-                        .nIn(numHiddenNodes).nOut(numOutputs).build())
-                .pretrain(false).backprop(true).build();
-
-
-        MultiLayerNetwork model = new MultiLayerNetwork(conf);
-        model.init();
-        model.setListeners(new ScoreIterationListener(10));  //Print score every 10 parameter updates
-        System.out.println("network is built");
-        
-        final String filenameTest  = new ClassPathResource("F:\\dl4j_data\\linear_data_eval.csv").getFile().getPath();
-        final String filenameTrain  = new ClassPathResource("F:\\dl4j_data\\linear_data_train.csv").getFile().getPath();
-
-        //Load the training data:
-        RecordReader rr = new CSVRecordReader();
-//        rr.initialize(new FileSplit(new File("src/main/resources/classification/linear_data_train.csv")));
-        rr.initialize(new FileSplit(new File(filenameTrain)));
-        DataSetIterator trainIter = new RecordReaderDataSetIterator(rr,batchSize,0,2);
-
-        //Load the test/evaluation data:
-        RecordReader rrTest = new CSVRecordReader();
-        rrTest.initialize(new FileSplit(new File(filenameTest)));
-        DataSetIterator testIter = new RecordReaderDataSetIterator(rrTest,batchSize,0,2);
-
-        
-
-        for ( int n = 0; n < nEpochs; n++) {
-            model.fit( trainIter );
-        }
-
-        System.out.println("Evaluate model....");
-        Evaluation eval = new Evaluation(numOutputs);
-        while(testIter.hasNext()){
-            DataSet t = testIter.next();
-            INDArray features = t.getFeatureMatrix();
-            INDArray lables = t.getLabels();
-            INDArray predicted = model.output(features,false);
-
-            eval.eval(lables, predicted);
-
-        }
-
-        //Print the evaluation statistics
-        System.out.println(eval.stats());
-
-
-    }
+public class example_network {
+	
+	public static void main(String args[]) throws Exception{
+		// the parameters for the net conf
+		int seed = 100;
+		double learn_rate = 0.01;
+		int batch_size = 50;
+		int nEpoch = 30;
+		int num_node_input = 2; 
+		//the number of features
+		
+		int num_node_output = 1;
+		// the number of node output from network
+		
+		// let's build the network configuration
+		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+				.seed(seed)
+				.iterations(2)
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.learningRate(learn_rate)
+				.updater(Updater.NESTEROVS)
+				.list()
+				.layer(0, new DenseLayer.Builder()
+								.nIn(num_node_input)
+								.nOut(5)
+								.weightInit(WeightInit.XAVIER)
+								.activation(Activation.RELU)
+								.build()
+								)
+				.layer(1, new DenseLayer.Builder()
+						.nIn(5)
+						.nOut(num_node_output)
+						.weightInit(WeightInit.XAVIER)
+						.activation(Activation.RELU)
+						.build()
+						)
+				.pretrain(false).backprop(true).build();
+		/*
+		 * it is a very sample network 
+		 * 2 nodes -> hidden 5 nodes -> 1 nodes
+		 */
+		
+		/* when we have the conf of the network , we can use it to build a real 
+		 * netwoek model 
+		 */
+		MultiLayerNetwork model = new MultiLayerNetwork(conf);
+		model.init();
+		
+		model.setListeners(new ScoreIterationListener(10));
+		//output the score of model every 10 iterations of training
+		System.out.println("the model is built !");
+		
+		
+		//all right, let us read the data from location 
+		//final String filenameTest = new ClassPathResource("e:\\linear_data_eval.csv").getFile().getPath();
+		//final String filenameTrain = new ClassPathResource("e:\\linear_data_train.csv").getFile().getPath();
+		
+		//load the data from the file 
+		RecordReader rr = new CSVRecordReader();
+		rr.initialize(new FileSplit(new File("d:\\linear_data_eval.csv")));
+		DataSetIterator train = new RecordReaderDataSetIterator(rr, batch_size, 0,2);
+		
+		RecordReader rt = new CSVRecordReader();
+		rt.initialize(new FileSplit(new File("d:\\linear_data_train.csv")));
+		DataSetIterator test = new RecordReaderDataSetIterator(rt, batch_size, 0,2);
+		System.out.println("we get the load location");
+		
+		for (int n = 0; n < nEpoch ; n++){
+			System.out.println("it is the "+n+" train step==========");
+			model.fit(train);
+		}
+		
+		System.out.println("the model has been trained !");
+		
+		
+		// let us evaluation the model 
+		Evaluation eval = new Evaluation(num_node_output);
+		
+		while (test.hasNext()){
+			DataSet t = test.next();
+			INDArray fea = t.getFeatureMatrix();
+			INDArray labels = t.getLabels();
+			INDArray pred = model.output(fea, false);
+			
+			eval.eval(labels, pred);
+			
+		}
+		
+		System.out.println(eval.stats());
+		
+		System.out.println("the program is over");
+		
+	}
 }
